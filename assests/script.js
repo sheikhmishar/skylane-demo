515a3e38406642fa623c6188f9618abab866e7f1
whoafridi <afridirk10@gmail.com>
2023-04-19 00:44:17 +0600

    first commit

---
+++ assests/script.js
@@ -0,0 +1,141 @@
+window.onload = function () {
+  let bar = document.querySelectorAll('.bar');
+  bar.forEach((progress) => {
+    let value = progress.getAttribute('data-value');
+    progress.style.width = `${value}%`;
+    let count = 0;
+    let progressAnimation = setInterval(() => {
+      count++;
+      progress.setAttribute('data-text', `${count}%`);
+      if (count >= value) {
+        clearInterval(progressAnimation);
+      }
+    }, 15);
+  });
+};
+
+$(document).ready(function(){
+    // Initialize the Slick Carousel      
+    $('.responsive').slick({
+dots: true,
+infinite: true,
+speed: 300,
+slidesToShow: 1,
+slidesToScroll: 1,
+autoplay: true,
+arrows: false,
+autoplaySpeed: 2000,
+responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+      dots: true
+    }
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+]
+});
+  });
+
+  $(document).ready(function(){
+    // Initialize the Slick Carousel      
+    $('.testimonial').slick({
+dots: true,
+infinite: true,
+speed: 300,
+slidesToShow: 4,
+slidesToScroll: 1,
+autoplay: true,
+arrows: false,
+autoplaySpeed: 2000,
+responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+      dots: true
+    }
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+]
+});
+  });
+
+  $(document).ready(function(){
+    // Initialize the Slick Carousel      
+    $('.univ').slick({
+dots: true,
+infinite: true,
+speed: 300,
+slidesToShow: 4,
+slidesToScroll: 1,
+autoplay: true,
+arrows: false,
+autoplaySpeed: 2000,
+responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+      dots: true
+    }
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+]
+});
+  });
