52ac21a2d3da8dcdb399645c715354ffa7abf4de
whoafridi <afridirk10@gmail.com>
2023-05-12 23:48:29 +0600

    about us

---
+++ assests/js/script.js
@@ -0,0 +1,172 @@
+window.onload = function () {
+  let bar = document.querySelectorAll(".bar");
+  bar.forEach((progress) => {
+    let value = progress.getAttribute("data-value");
+    progress.style.width = `${value}%`;
+    let count = 0;
+    let progressAnimation = setInterval(() => {
+      count++;
+      progress.setAttribute("data-text", `${count}%`);
+      if (count >= value) {
+        clearInterval(progressAnimation);
+      }
+    }, 15);
+  });
+};
+
+// navbar sticky
+const navel = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+  if (scrollY < 56) {
+    navel.classList.remove("navbar-scrolled");
+    navel.classList.add("navbar-margin");
+  }
+
+  if (window.scrollY > 60) {
+    navel.classList.add("navbar-scrolled");
+    navel.classList.remove("navbar-margin");
+  }
+});
+
+$(document).ready(function () {
+  // Initialize the Slick Carousel
+  $(".responsive").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
+
+$(document).ready(function () {
+  // Initialize the Slick Carousel
+  $(".testimonial").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
+
+$(document).ready(function () {
+  // Initialize the Slick Carousel
+  $(".univ").slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
+
+// about count up 
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue +=1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
