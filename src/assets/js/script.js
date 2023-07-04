60b01991c79efe9ac171f8ad48ec2393d709d9c7
whoafridi <afridirk10@gmail.com>
2023-07-04 23:15:49 +0600

    updated mobile view

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -193,6 +193,45 @@ $(document).ready(function ($) {
   });
 });
 
+$(document).ready(function ($) {
+  // Initialize the Slick Carousel
+  $(".services-mobile").slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    arrows: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
+
 // about count up
 let valueDisplays = document.querySelectorAll(".num");
 let interval = 4000;
